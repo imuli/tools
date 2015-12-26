0af9d4aab9507c19967b811c20503bfae21c04d9
Imuli <i@imu.li>
2015-12-25 22:00:43 -0500

    updated lines to copy from start to end

--- lines.c
+++ lines.c
@@ -1,32 +1,40 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-const char usage[] = "usage: lines (lines)\n";
+const char usage[] = "usage: lines {start} (end)\n";
 
 int
 main(int argc, char **argv){
-	int lines, count, more;
+	int start, lines, count, offset, more;
 	char buf[512];
 
+	start = 1;
+	if(argc == 3) {
+		start = atoi(argv[1]);
+		argc--;
+		argv++;
+	}
 	if(argc != 2) {
 		write(2, usage, sizeof(usage));
 		return 1;
 	}
 	lines = atoi(argv[1]);
 
-	count = 0;
-	more = 1;
-	while(lines > 0 && more){
-		more = read(0, buf+count, 1);
-		lines -= (buf[count] == '\n');
-		count += more;
-		if(count == sizeof(buf)){
-			write(1, buf, count);
-			count=0;
+	for(more = 1, count = 0; count < lines && more;){
+		for(offset = 0; offset < sizeof(buf);){
+			more = read(0, buf+offset, 1);
+
+			if(!more) /* eof */
+				break;
+
+			offset++;
+			if(buf[offset-1] == '\n'){
+				count++;
+			   	break;
+			}
 		}
+		if(count >= start)
+			write(1, buf, offset);
 	}
-	if(lines == 0 || !more)
-		write(1, buf, count);
 	return 0;
 }
-
